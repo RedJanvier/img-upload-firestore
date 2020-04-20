f1885e8eeb752353a87d461ac2b399e40c20c780
RedJanvier <janvierntwali@gmail.com>
2020-04-20 15:13:59 +0200

    feat(post): stored post with multiple imgs

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 const form = document.querySelector(".upload-form");
 const errorBox = document.querySelector(".error-box");
 const successBox = document.querySelector(".success-box");
-const previewImg = document.querySelector(".upload-preview img");
+const previewSect = document.querySelector(".upload-preview");
 const previewTxt = document.querySelector(".upload-preview span");
 
-const storePost = async (url) => {
+const storePost = async (urls) => {
   const post = {
     title: form.title.value,
     description: form.description.value,
-    image: `${url}`,
   };
+
+  urls.forEach((url, i) => {
+    i > 0 ? (post[`image${i}`] = url) : (post["image"] = url);
+  });
 
   try {
     const snapshot = await db.collection("posts").add(post);
@@ -20,61 +23,88 @@ const storePost = async (url) => {
 };
 
 form.image.addEventListener("change", () => {
-  const file = form.image.files[0];
+  const { files } = form.image;
 
-  if (file) {
-    const reader = new FileReader();
+  switch (files.length) {
+    case 2:
+      previewSect.classList.add("double");
+      break;
+    case 3:
+      previewSect.classList.add("triple");
+      break;
+    default:
+      break;
+  }
 
-    previewTxt.style.display = "none";
+  previewSect.innerHTML = "";
+  previewSect.style.border = "none";
 
-    reader.addEventListener("load", () => {
-      previewImg.style.width = "100%";
-      previewImg.parentElement.style.borderColor = "green";
-      previewImg.setAttribute("src", reader.result);
-    });
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
 
-    reader.readAsDataURL(file);
+    if (file) {
+      const reader = new FileReader();
+
+      previewTxt.style.display = "none";
+
+      reader.addEventListener("load", () => {
+        const imageWrapper = document.createElement("div");
+        const preview = document.createElement("img");
+        preview.setAttribute("src", reader.result);
+        imageWrapper.setAttribute("class", "image");
+
+        imageWrapper.appendChild(preview);
+        return previewSect.appendChild(imageWrapper);
+      });
+
+      reader.readAsDataURL(file);
+    }
   }
 });
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const image = form.image.files[0];
+  const images = form.image.files;
+  const downloadURLs = [];
+  for (let i = 0; i < images.length; i++) {
+    if (images[i]) {
+      const file = images[i];
+      const metadata = {
+        contentType: "image/jpeg",
+      };
+      const uploadTask = storage
+        .ref()
+        .child("images/" + file.name)
+        .put(file, metadata);
 
-  if (image) {
-    const file = image;
-    const metadata = {
-      contentType: "image/jpeg",
-    };
-    const uploadTask = storage
-      .ref()
-      .child("images/" + file.name)
-      .put(file, metadata);
-
-    // Listen for state changes, errors, and completion of the upload.
-    uploadTask.on(
-      firebase.storage.TaskEvent.STATE_CHANGED,
-      null,
-      null,
-      function () {
-        // Upload completed successfully, now we can get the download URL
-        uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
-          successBox.style.display = "block";
-          //   const preview = document.createElement("img");
-          //   preview.setAttribute("src", downloadURL);
-          //   successBox.appendChild(preview);
-          storePost(downloadURL);
-          setTimeout(() => (successBox.style.display = "none"), 5000);
-        });
-      }
-    );
-  } else {
-    const error = document.createElement("p");
-    error.textContent = "Please select a file!";
-    errorBox.innerHTML = "";
-    errorBox.appendChild(error);
-    errorBox.style.display = "block";
-    setTimeout(() => (errorBox.style.display = "none"), 5000);
+      // Listen for state changes, errors, and completion of the upload.
+      uploadTask.on(
+        firebase.storage.TaskEvent.STATE_CHANGED,
+        null,
+        null,
+        function () {
+          // Upload completed successfully, now we can get the download URL
+          uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+            successBox.style.display = "block";
+            //   const preview = document.createElement("img");
+            //   preview.setAttribute("src", downloadURL);
+            //   successBox.appendChild(preview);
+            downloadURLs.push(downloadURL);
+            if (i == images.length - 1) {
+              storePost(downloadURLs);
+            }
+            setTimeout(() => (successBox.style.display = "none"), 5000);
+          });
+        }
+      );
+    } else {
+      const error = document.createElement("p");
+      error.textContent = "Please select a file!";
+      errorBox.innerHTML = "";
+      errorBox.appendChild(error);
+      errorBox.style.display = "block";
+      setTimeout(() => (errorBox.style.display = "none"), 5000);
+    }
   }
 });
